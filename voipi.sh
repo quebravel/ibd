de03bee98cd0b9796165c40bfa4208b78f9f4ae3
Jonatas <jonatasgb@gmail.com>
2019-06-26 01:07:34 -0300

    Update voipi.sh

--- voipi.sh
+++ voipi.sh
@@ -4,7 +4,7 @@ _sx="sudo xbps-install"
 
 $_sx -Su
 
-$_sx xsetroot mpv feh ranger redshift xset xrdb xsel python3-neovim bspwm sxhkd polybar git wget ntfs-3g xorg-minimal xorg-fonts xf86-video-amdgpu rxvt-unicode urxvt-perls xf86-input-{evdev,joystick,libinput} libEGL curl alsa-utils pulseaudio pulsemixer w3m-img numlockx xsel zathura-pdf-poppler
+$_sx scrot xsetroot mpv feh ranger redshift xset xrdb xsel python3-neovim bspwm sxhkd polybar git wget ntfs-3g xorg-minimal xorg-fonts xf86-video-amdgpu rxvt-unicode urxvt-perls xf86-input-{evdev,joystick,libinput} libEGL curl alsa-utils pulseaudio pulsemixer w3m-img numlockx xsel zathura-pdf-poppler
 
 $_sx xcursor-vanilla-dmz-aa firefox-i18n-pt-BR firefox google-fonts-ttf
 
