254b8864c9f901753c123d6b347076fcebca1151
Jonatas <jonatasgb@gmail.com>
2019-07-30 11:49:14 -0300

    Update voipi.sh

--- voipi.sh
+++ voipi.sh
@@ -2,24 +2,36 @@
 
 _sx="sudo xbps-install"
 
-$_sx -Su
+$_sx -Suy
 
-$_sx scrot xsetroot mpv feh ranger redshift xset xrdb xsel python3-neovim bspwm sxhkd polybar git wget ntfs-3g xorg-minimal /
-xorg-fonts xf86-video-amdgpu rxvt-unicode urxvt-perls xf86-input-{evdev,joystick,libinput} libEGL curl alsa-utils w3m-img /
-numlockx zathura-pdf-poppler adwaita-icon-theme
+# --- PROGRAMAS ---
+$_sx -y scrot xsetroot mpv feh ranger redshift xset xrdb xsel python3-neovim git wget ntfs-3g xorg-minimal xorg-fonts rxvt-unicode urxvt-perls xf86-input-{evdev,joystick,libinput} libEGL curl alsa-utils w3m-img numlockx zathura-pdf-poppler adwaita-icon-theme neofetch htop
 
+# --- AUDIO ---
 #$_sx pulseaudio pamixer
 
-$_sx xcursor-vanilla-dmz-aa firefox-i18n-pt-BR firefox google-fonts-ttf
+# --- VIDEO ---
+#$_sx xf86-video-amdgpu
+#$_sx xf86-video-intel
+#$_sx xf86-video-nvidia
 
-#$_sx unclutter
+# --- WINDOW MANAGER ---
+#$_sx bspwm sxhkd polybar unclutter
+#$_sx awesome unclutter
 
+# --- NAVEGADOR ---
+$_sx xcursor-vanilla-dmz-aa
+#$_sx firefox-i18n-pt-BR firefox
+#$_sx qutebrowser
+
+# --- REMOVER BIP ---
 echo 'blacklist pcspkr' | sudo tee /etc/modprobe.d/blacklist.conf
 
-sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
+# --- CONFIGURAR FONTES ---
+
+$_sx google-fonts-ttf
 sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
 sudo ln -s /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
-sudo ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d/
 sudo ln -s /usr/share/fontconfig/conf.avail/70-yes-bitmaps.conf /etc/fonts/conf.d/
 
 exit 0
