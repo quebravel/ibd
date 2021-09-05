674b5813593755c98a563658a4f544f8da3f5416
quebravel <jonatasgb@gmail.com>
2021-09-05 14:27:44 -0300

    upgrade2.1

--- voipi.sh
+++ voipi.sh
@@ -33,7 +33,7 @@ pulse(){
 
 # --- NAVEGADOR ---
 navegador(){
-#    $_sx qutebrowser
+    $_sx qutebrowser
     echo "Adicionando dicionário"
     /usr/share/qutebrowser/scripts/dictcli.py install pt-BR
 }
